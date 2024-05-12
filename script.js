6daa97b3d73e5c33ad8aa27ce2362aecf692c082
praveenprk <praveenkumarkp666@gmail.com>
2024-05-13 02:21:01 +0530

    add more event handlers and styles

--- script.js
+++ script.js
@@ -7,8 +7,19 @@ const the_what_1 = document.getElementById("the_what_1");
 const what_2 = document.getElementById("what_2");
 const the_what_2 = document.getElementById("the_what_2");
 
+const what_3 = document.getElementById("what_3");
+const the_what_3 = document.getElementById("the_what_3");
+
 const my_journey_2 = document.getElementById("my_journey_2");
 const the_who_2 = document.getElementById("the_who_2");
+
+const fun_1 = document.getElementById("fun_1");
+const fun_2 = document.getElementById("fun_2");
+const fun_3 = document.getElementById("fun_3");
+
+const the_fun_1 = document.getElementById("the_fun_1");
+const the_fun_2 = document.getElementById("the_fun_2");
+const the_fun_3 = document.getElementById("the_fun_3");
 
 const show = (elem) => elem.style.display = "block";
 const hide = (elem) => elem.style.display = "none";
@@ -18,8 +29,17 @@ const hide = (elem) => elem.style.display = "none";
 } */
 
 window.addEventListener("load", () => {
-    console.log(`onload:`);
-    [who, the_what_1, the_what_2, the_who_2].map((e) => {
+    console.log(`onload`);
+    [
+        who, 
+        the_what_1,
+        the_what_2,
+        the_what_3,
+        the_who_2,
+        the_fun_1,
+        the_fun_2,
+        the_fun_3
+    ].map((e) => {
         e.style.display = "none";
     });
 });
@@ -54,4 +74,36 @@ what_2.addEventListener("mouseover", () => {
 
 what_2.addEventListener("mouseout", () => {
     hide(the_what_2);
+});
+
+what_3.addEventListener("mouseover", () => {
+    show(the_what_3);
+});
+
+what_3.addEventListener("mouseout", () => {
+    hide(the_what_3);
+});
+
+fun_1.addEventListener("mouseover", () => {
+    show(the_fun_1);
+});
+
+fun_1.addEventListener("mouseout", () => {
+    hide(the_fun_1);
+});
+
+fun_2.addEventListener("mouseover", () => {
+    show(the_fun_2);
+});
+
+fun_2.addEventListener("mouseout", () => {
+    hide(the_fun_2);
+});
+
+fun_3.addEventListener("mouseover", () => {
+    show(the_fun_3);
+});
+
+fun_3.addEventListener("mouseout", () => {
+    hide(the_fun_3);
 });
